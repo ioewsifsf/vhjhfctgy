7689bebcc826e7182b3a9525ee996b7cd3d46a60
ioewsifsf <130152891+ioewsifsf@users.noreply.github.com>
2023-04-13 21:33:34 +0800

    Add files via upload

---
+++ 2048/main.py
@@ -0,0 +1,24 @@
+import configs
+from Game2048 import Game2048
+
+
+def main(args):
+    """
+    screen_width: Width of the form
+    screen_height: Height of the form
+    block_gap: Gap between two blocks
+    block_size: Size of a block
+    """
+    screen_width = args.screen_width
+    screen_height = args.screen_height
+    block_gap = args.block_gap
+    block_size = args.block_size
+    block_arc = args.block_arc
+
+    game = Game2048(screen_width, screen_height, block_gap, block_size, block_arc)
+    game.Form()
+
+
+if __name__ == '__main__':
+    args = configs.parse_args()
+    main(args)
